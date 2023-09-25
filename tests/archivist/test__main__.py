c914e7de7c34c9888385925092f9180443696bcb
Brad Edwards <brade@ensemble.com>
2023-09-24 20:19:41 -0700

    Fix main unit tests work

--- tests/archivist/test__main__.py
+++ tests/archivist/test__main__.py
@@ -17,43 +17,39 @@ test__main__.py
 """
 import unittest
 from unittest.mock import patch, Mock
-from src.archivist.__main__ import main, Config
+from src.archivist.__main__ import main
+
 
 class TestArchivist(unittest.TestCase):
 
-    @patch('src.archivist.__main__.ArgumentParser.parse_args')  # Mocking argparse's parse_args method
-    @patch('src.archivist.__main__.subprocess.run')  # Mocking subprocess.run method
-    @patch('src.archivist.__main__.sys.exit')  # Mocking sys.exit
-    def test_version_flag(self, mock_exit, mock_run, mock_parse_args):
-        mock_args = Mock()
-        mock_args.version = True
-        mock_parse_args.return_value = mock_args
-
-        main()
-
-        mock_exit.assert_called_once_with(0)
-
+    @patch('src.archivist.__main__.Crawler')
     @patch('src.archivist.__main__.DEBUG', new_callable=Mock)
     @patch('src.archivist.__main__.argparse.ArgumentParser.parse_args')
     @patch('src.archivist.__main__.subprocess.run')
     @patch('src.archivist.__main__.sys.exit')
-    def test_update_flag(self, mock_exit, mock_run, mock_parse_args, mock_debug):
+    def test_update_flag(self, mock_exit, mock_run, mock_parse_args, mock_debug, mocked_crawler):
         mock_debug.return_value = False
         mock_args = Mock()
         mock_args.update = True
         mock_args.version = False
         mock_parse_args.return_value = mock_args
 
+        # Setup the mock Crawler instance
+        mock_crawler_instance = Mock()
+        mock_crawler_instance.repo_url = 'https://mocked.repo.url'
+        mocked_crawler.return_value = mock_crawler_instance
+
         main()
 
         mock_run.assert_called_once()
         mock_exit.assert_called_once_with(0)
 
+    @patch('src.archivist.__main__.Crawler')
     @patch('src.archivist.__main__.DEBUG', new_callable=Mock)
     @patch('argparse.ArgumentParser.parse_args')
     @patch('subprocess.run')
     @patch('sys.exit')
-    def test_version_flag(self, mock_exit, mock_run, mock_parse_args, mock_debug):
+    def test_version_flag(self, mock_exit, mock_run, mock_parse_args, mock_debug, mocked_crawler):
         mock_debug.return_value = False
         mock_args = Mock()
         mock_args.github_url = "some_url"
@@ -62,14 +58,16 @@ class TestArchivist(unittest.TestCase):
         mock_args.update = False
         mock_parse_args.return_value = mock_args
 
-        main()
+        # Setup the mock Crawler instance
+        mock_crawler_instance = Mock()
+        mocked_crawler.return_value = mock_crawler_instance
 
-        config = Config(github_url=mock_args.github_url, output_path=mock_args.output_path,
-                        branch=mock_args.branch, verbose=mock_args.verbose, quiet=mock_args.quiet,
-                        token=mock_args.token, config_file=mock_args.config,
-                        embeddings_path=mock_args.embeddings_path)
-        self.assertEqual(config.github_url, "some_url")
-        self.assertEqual(config.output_path, "some_path")
+        # Optionally, set return values or side effects for methods in mock_crawler_instance
+        # For example, mock_crawler_instance.execute.return_value = "some_value"
+
+        main()
+        self.assertEqual(mock_args.github_url, "some_url")
+        self.assertEqual(mock_args.output_path, "some_path")
 
 
 if __name__ == '__main__':
